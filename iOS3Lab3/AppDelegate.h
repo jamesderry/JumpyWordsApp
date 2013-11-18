58709a03453ec810d3644b66351b779a826cd6a8
James Derry <jderry@derryitconsulting.com>
2013-11-18 16:20:26 -0600

    Initial Commit

---
+++ iOS3Lab3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS3Lab3
+//
+//  Created by James Derry on 11/18/13.
+//  Copyright (c) 2013 James Derry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
